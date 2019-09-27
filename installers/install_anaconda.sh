3f500cbe8fadfbf5769b6014143408cfbe6cefd3
Matthew Brett <matthew.brett@gmail.com>
2019-09-27 16:26:19 +0100

    Another tack on Anaconda install

--- installers/install_anaconda.sh
+++ installers/install_anaconda.sh
@@ -1,8 +1,13 @@
 # Installer from:
 # https://repo.anaconda.com/archive/Anaconda3-2019.07-Linux-x86_64.sh
 
+ANACONDA_DIR=${DS_HOME}/anaconda3
 sudo -u ${DS_USER} mkdir /home/${DS_USER}/.conda
-sudo -u ${DS_USER} sh ${SCRIPT_DIR}/Anaconda*.sh -b -p ${DS_HOME}/anaconda3
+# We apparently need root to run the installer, at this stage.
+# Otherwise I get a permission error in multiprocessing.
+sh ${SCRIPT_DIR}/Anaconda*.sh -b -p ${ANACONDA_DIR}
+# Set permissions on directory.
+chown -R ${DS_USER}:${DS_USER} ${ANACONDA_DIR}
 sudo echo 'export PATH=$HOME/anaconda3/bin:$PATH' >> ${DS_HOME}/.bashrc
 cp ${SCRIPT_DIR}/Desktop/jupyter.desktop ${DS_DESKTOP}
 sudo -u ${DS_USER} mkdir ${DS_DESKTOP}/Notebooks
--- installers/post_install.sh
+++ installers/post_install.sh
@@ -9,18 +9,21 @@ sudo apt -y upgrade
 sudo apt -y full-upgrade
 sudo apt -y autoremove
 
-# Make desktop directory
+# Make desktop and downloads directory
 DS_HOME="/home/${DS_USER}"
 DS_DESKTOP="${DS_HOME}/Desktop"
+DS_DOWNLOADS="${DS_HOME}/Downloads"
 sudo -u ${DS_USER} mkdir ${DS_DESKTOP}
+sudo -u ${DS_USER} mkdir ${DS_DOWNLOADS}
 
 # Copy generic shortcuts
 cp ${SCRIPT_DIR}/Desktop/firefox.desktop ${DS_DESKTOP}
 cp ${SCRIPT_DIR}/Desktop/lxterminal.desktop ${DS_DESKTOP}
 cp ${SCRIPT_DIR}/Desktop/fileman.desktop ${DS_DESKTOP}
 cp ${SCRIPT_DIR}/Desktop/connecting_to_wifi.pdf ${DS_DESKTOP}
+
 # Make shortcut to wifi certificate
-sudo -u ${DS_USER} ln -s /etc/ssl/certs/QuoVadis_Root_CA_2.pem ${DS_HOME}/Downloads/
+sudo -u ${DS_USER} ln -s /etc/ssl/certs/QuoVadis_Root_CA_2.pem ${DS_DOWNLOADS}
 
 source ${SCRIPT_DIR}/install_anaconda.sh
 
